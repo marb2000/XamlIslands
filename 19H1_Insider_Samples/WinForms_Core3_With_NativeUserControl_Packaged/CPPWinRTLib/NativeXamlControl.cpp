4b8f7e1345baedb4c4521e91ad4a4d6c3cb3dde6
Miguel Ramos <miguelrb@microsoft.com>
2019-01-15 16:44:09 -0800

    Adding WinForms consuming a NativeXamlComponent

---
+++ 19H1_Insider_Samples/WinForms_Core3_With_NativeUserControl_Packaged/CPPWinRTLib/NativeXamlControl.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "NativeXamlControl.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::CPPWinRTLib::implementation
+{
+    NativeXamlControl::NativeXamlControl()
+    {
+        InitializeComponent();
+    }
+
+    int32_t NativeXamlControl::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void NativeXamlControl::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void NativeXamlControl::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
---
+++ 19H1_Insider_Samples/WinForms_Core3_With_NativeUserControl_Packaged/CPPWinRTLib/pch.h
@@ -0,0 +1,18 @@
+﻿//
+// pch.h
+// Header for platform projection include files
+//
+
+#pragma once
+#include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.ApplicationModel.Activation.h"
+#include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
+#include "winrt/Windows.ApplicationModel.Activation.h"
+#include "winrt/Windows.UI.Xaml.Controls.h"
+#include "winrt/Windows.UI.Xaml.Media.h"
+#include "winrt/Windows.Storage.Streams.h"
+#include "winrt/Windows.Graphics.Imaging.h"
+#include "winrt/Windows.Media.Ocr.h"
+#include "winrt/Windows.Storage.Pickers.h"
+#include "winrt/Windows.Networking.h"
+
