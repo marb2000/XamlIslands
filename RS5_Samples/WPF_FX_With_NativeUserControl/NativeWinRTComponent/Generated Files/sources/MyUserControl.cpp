914810d5548f9876161cef74dda7e807f4fed2d5
Miguel Ramos <miguelrb@microsoft.com>
2019-01-22 16:23:23 -0800

    Updating samples

---
+++ RS5_Samples/WPF_FX_With_NativeUserControl/NativeWinRTComponent/Generated Files/sources/MyUserControl.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "MyUserControl.h"
+
+namespace winrt::NativeWinRTComponent::implementation
+{
+    int32_t MyUserControl::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MyUserControl::MyProperty(int32_t value)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ RS5_Samples/WPF_FX_With_NativeUserControl/NativeWinRTComponent/MyUserControl.h
@@ -0,0 +1,27 @@
+﻿//
+// Declaration of the MyUserControl class.
+//
+
+#pragma once
+
+#include "MyUserControl.g.h"
+
+namespace winrt::NativeWinRTComponent::implementation
+{
+    struct MyUserControl : MyUserControlT<MyUserControl>
+    {
+        MyUserControl();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::NativeWinRTComponent::factory_implementation
+{
+    struct MyUserControl : MyUserControlT<MyUserControl, implementation::MyUserControl>
+    {
+    };
+}
