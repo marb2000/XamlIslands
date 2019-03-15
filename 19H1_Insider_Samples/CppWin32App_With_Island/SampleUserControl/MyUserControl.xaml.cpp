caf1c4bd09ae9b971ebfd4104878babf815c9056
Oscar Calvo <ocalvo@microsoft.com>
2019-03-15 13:56:11 -0700

    Initial commit

---
+++ 19H1_Insider_Samples/CppWin32App_With_Island/SampleUserControl/MyUserControl.xaml.cpp
@@ -0,0 +1,27 @@
+﻿//
+// MyUserControl.xaml.cpp
+// Implementation of the MyUserControl class
+//
+
+#include "pch.h"
+#include "MyUserControl.xaml.h"
+
+using namespace SampleUserControl;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+MyUserControl::MyUserControl()
+{
+	InitializeComponent();
+}
