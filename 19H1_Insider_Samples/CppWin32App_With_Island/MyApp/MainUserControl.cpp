83e148f2b69cea02e1e818f630d4df431d9eab9f
Oscar Calvo <ocalvo@microsoft.com>
2019-04-03 15:23:17 -0700

    Gets the App project to produce the resources.pri file

--- 19H1_Insider_Samples/CppWin32App_With_Island/MyApp/MainUserControl.cpp
+++ 19H1_Insider_Samples/CppWin32App_With_Island/MyApp/MainUserControl.cpp
@@ -8,12 +8,6 @@ namespace winrt::MyApp::implementation
 {
     MainUserControl::MainUserControl()
     {
-//        if (!_contentLoaded)
-//        {
-//            _contentLoaded = true;
-//            Windows::Foundation::Uri resourceLocator{ L"ms-appx:///MainUserControl.xaml" };
-//            Windows::UI::Xaml::Application::LoadComponent(*this, resourceLocator, Windows::UI::Xaml::Controls::Primitives::ComponentResourceLocation::Nested);
-//        }
         InitializeComponent();
     }
 }
