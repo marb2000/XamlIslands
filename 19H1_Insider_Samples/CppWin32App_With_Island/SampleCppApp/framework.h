0009da5accf78efd371411346047e9bb1df5bcce
Oscar Calvo <ocalvo@microsoft.com>
2019-04-09 15:57:41 -0700

    Update to latest CPP\WinRT

--- 19H1_Insider_Samples/CppWin32App_With_Island/SampleCppApp/framework.h
+++ 19H1_Insider_Samples/CppWin32App_With_Island/SampleCppApp/framework.h
@@ -1,17 +1,12 @@
 #pragma once
 
 #include <unknwn.h> // To enable support for non-WinRT interfaces, unknwn.h must be included before any C++/WinRT headers.
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/MyApp.h>
+#include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
 #include <winrt/Windows.system.h>
 #include <winrt/windows.ui.xaml.hosting.h>
-#include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
-#include <winrt/windows.ui.xaml.controls.h>
-#include <winrt/Windows.ui.xaml.media.h>
-
-#include <winrt/Microsoft.UI.Xaml.Markup.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.h>
-#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
-#include <winrt/SampleUserControl.h>
-#include <winrt/MyApp.h>
 
 #include <roapi.h>
 
