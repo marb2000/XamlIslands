d8885d41a14a97771317632dea2d6b40637da6e9
Oscar Calvo <ocalvo@microsoft.com>
2019-04-19 16:45:15 -0700

    Revoke Island events

--- 19H1_Insider_Samples/CppWin32App_With_Island/SampleCppApp/XamlBridge.h
+++ 19H1_Insider_Samples/CppWin32App_With_Island/SampleCppApp/XamlBridge.h
@@ -25,6 +25,7 @@ protected:
     HWND m_hMainWnd = nullptr;
 private:
     winrt::guid lastFocusRequestId;
+    std::vector<winrt::Windows::UI::Xaml::Hosting::DesktopWindowXamlSource::TakeFocusRequested_revoker> m_takeFocusEventRevokers;
     std::vector<winrt::Windows::UI::Xaml::Hosting::DesktopWindowXamlSource> m_xamlSources;
 };
 
