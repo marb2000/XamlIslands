1778191bdd5bf5a87c0199c514d4b557e689878e
Miguel Ramos <miguelrb@microsoft.com>
2019-04-24 17:14:12 -0700

    Updating to XamlHost 6.0.0-preview4

--- 19H1_Insider_Samples/WPF_Core3_With_UserControl/WPFCore3App/App.xaml.cs
+++ 19H1_Insider_Samples/WPF_Core3_With_UserControl/WPFCore3App/App.xaml.cs
@@ -13,5 +13,17 @@ namespace MyCoreConsole
     /// </summary>
     public partial class App : Application
     {
+        App()
+        {
+            InitializeComponent();
+            // Workaround in DotNET Core preview4
+            // An static field is not initialized: Microsoft.Toolkit.Wpf.UI.XamlHost.WindowsXamlHostBase._metadataContainer
+            // Here: https://github.com/windows-toolkit/Microsoft.Toolkit.Win32/blob/master/Microsoft.Toolkit.Wpf.UI.XamlHost/WindowsXamlHostBase.cs#L30
+
+            System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(typeof(Microsoft.Toolkit.Wpf.UI.XamlHost.WindowsXamlHostBase).TypeHandle);
+
+        }
+
+
     }
 }
