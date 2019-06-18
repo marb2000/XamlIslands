bfa76cd41ee83daa0ec89c4999699172c9a91eaa
Miguel Ramos <miguelrb@microsoft.com>
2019-06-18 15:20:48 -0700

    Samples for 1903

---
+++ 1903_Samples/Shared_Components/Managed_WinRT_Comp/ButtonPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MyUWPLib
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ButtonPage : Page
+    {
+        public ButtonPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+
+            if (b != null)
+            {
+                string name = b.Name;
+
+                switch (name)
+                {
+                    case "Button1":
+                        Control1Output.Text = "You clicked: " + name;
+                        break;
+                    case "Button2":
+                        Control2Output.Text = "You clicked: " + name;
+                        break;
+                    case "Button3":
+                        Control3Output.Text = "You clicked: " + name;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 1903_Samples/WinForms_Core3_App/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.windowsXamlHost1.InitialTypeName = "Samples.ManagedWinRT.MainPage";
+        }
+    }
+}
