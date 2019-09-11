81df38646562591eb6f174a39e9c10171a5fe8a5
Oscar Calvo <ocalvo@microsoft.com>
2019-09-11 14:47:10 -0700

    Initial commit for single island sample

---
+++ 1903_Samples/CppWinRT_Win32_SingleIsland/MyApp/pch.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#define DISABLE_XAML_GENERATED_MAIN
+
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Navigation.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+#include <winrt/SampleUserControl.h>
