3da67e53c16f649ace472da8203954dd056aee93
Joseph Martinsen <josmarti@adobe.com>
2019-11-11 15:54:22 -0800

    added basic looping example

--- 1903_Samples/WinForms_Core3_App/MainForm.cs
+++ 1903_Samples/WinForms_Core3_App/MainForm.cs
@@ -12,12 +12,18 @@ namespace MyWinFormsApp
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        public MainForm(bool runTest = true)
         {
             InitializeComponent();
 
             this.windowsXamlHost1.InitialTypeName = "Samples.ManagedWinRT.MainPage";
+            this.runTest = runTest;
+
+            tonsOfMemory = new byte[1024 * 1024 * 100];
         }
+
+        public bool runTest;
+        byte[] tonsOfMemory;
 
         protected override void OnLayout(LayoutEventArgs levent)
         {
@@ -27,11 +33,30 @@ namespace MyWinFormsApp
             {
                 global::Samples.ManagedWinRT.MainPage.NewWinForm = MainForm.NewWinForm;
             }
+
+            if (runTest)
+            {
+                OpenWindows();
+            }
+        }
+
+        public async void OpenWindows()
+        {
+            var numWindows = 100;
+            MainForm form;
+
+            for(int i = 0; i < numWindows; i++)
+            {
+                form = new MainForm(false);
+                form.Show();
+                await Task.Delay(1000);
+                form.Close();
+            }
         }
 
         static public void NewWinForm()
         {
-            MainForm form = new MainForm();
+            MainForm form = new MainForm(false);
             form.Show();
         }
     }
